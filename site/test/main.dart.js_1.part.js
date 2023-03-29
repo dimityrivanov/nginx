5423e063c7c9d45fb3e65e3c99f5ddad63f720f2
Dimitar Ivanov <dimitarivanovit@gmail.com>
2023-03-29 15:01:32 +0300

    test

---
+++ site/test/main.dart.js_1.part.js
@@ -0,0 +1,13 @@
+self.$__dart_deferred_initializers__=self.$__dart_deferred_initializers__||Object.create(null)
+$__dart_deferred_initializers__.current=function(a,b,c,$){var A={
+adl(){return new A.zR(null)},
+zR:function zR(d){this.a=d}},B
+A=a.updateHolder(c[3],A)
+B=c[0]
+A.zR.prototype={
+V(d){var y=null
+return B.a0f(new B.zS("Test Page",y,y,y,y),y,y)}}
+var z=a.updateTypes([]);(function inheritance(){var y=a.inherit
+y(A.zR,B.aX)})()
+B.a5u(b.typeUniverse,JSON.parse('{"zR":{"aX":[],"u":[]}}'))}
+$__dart_deferred_initializers__["2mvvMDSUhxCegj0UNBEiz5S8vH4="] = $__dart_deferred_initializers__.current
