ff53d0ae2b1b97472a160e39fdbbb6478d66ca0d
Dimitar Ivanov <dimitarivanovit@gmail.com>
2023-04-11 08:30:29 +0300

    removed lokopld
    removed lokopld

--- site/lokopd/main.dart.js_26.part.js
+++
@@ -1,8 +0,0 @@
-self.$__dart_deferred_initializers__=self.$__dart_deferred_initializers__||Object.create(null)
-$__dart_deferred_initializers__.current=function(a,b,c,$){var A,B,C,D
-A=c[28]
-B=c[0]
-C=c[24]
-D=c[2]
-var z=a.updateTypes([]);(function constants(){A.fW=new B.cf(C.ek,null,null,null,null,null,D.cu)})()}
-$__dart_deferred_initializers__["EiN1kdIxCT8ZauWAOeKSv3KyS/I="] = $__dart_deferred_initializers__.current
--- site/lokopd/main.dart.js_6.part.js
+++
@@ -1,6 +0,0 @@
-self.$__dart_deferred_initializers__=self.$__dart_deferred_initializers__||Object.create(null)
-$__dart_deferred_initializers__.current=function(a,b,c,$){var A,B
-A=c[20]
-B=c[0]
-var z=a.updateTypes([]);(function constants(){A.ck=new B.bz(null,20,null,null)})()}
-$__dart_deferred_initializers__["Kkcmk3QPBYpc5e8zQgoXl3ruvHA="] = $__dart_deferred_initializers__.current
